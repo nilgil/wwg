b383f0e15a0c25196cddf658953bfa57205d1b1c
길재우 <6197@.>
2021-11-12 04:03:47 +0900

    기본적인 스프링 시큐리티 적용

--- src/main/java/com/project/wwg/secu/controller/SecuController.java
+++ src/main/java/com/project/wwg/secu/controller/SecuController.java
@@ -6,32 +6,36 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class TestController {
-    private static final Log LOG = LogFactory.getLog(TestController.class);
+public class SecuController {
+    private static final Log LOG = LogFactory.getLog(SecuController.class);
 
     TestService testService;
 
-    TestController(TestService testService){
+    SecuController(TestService testService){
         this.testService = testService;
     }
 
-    @GetMapping("/login")
+    @GetMapping("/loginPage")
     public String login(Model model){
         LOG.info("hello");
         model.addAttribute("content","testSuccess");
         return "secu/login";
     }
 
-    @GetMapping("users")
-    public String getUsers(
-            Model model,
-            @RequestParam("user")String user){
-        LOG.info("hello user");
-        model.addAttribute("user",testService.getUsers(user));
-        return "secu/dbTest";
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/all")
+    public String all() {
+        return "secu/all";
     }
 }
--- src/main/webapp/WEB-INF/views/secu/all.jsp
+++ src/main/webapp/WEB-INF/views/secu/all.jsp
@@ -2,6 +2,8 @@
          pageEncoding="utf-8" isELIgnored="false"%>
 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
@@ -9,11 +11,16 @@
 </head>
 <body>
 
-<form action="/login" method="post">
-    <input type="text" name="id" placeholder="아이디">
-    <input type="password" name="pw" placeholder="비밀번호">
+<h1>ADMIN 접근 가능페이지</h1>
+
+
+<sec:authorize access="isAuthenticated()">
+    <a href="#" onclick="document.getElementById('logout').submit();">로그아웃</a>
+</sec:authorize>
+
+
+<form id="logout" action="/logout" method="POST">
     <input name="${_csrf.parameterName}" type="hidden" value="${_csrf.token}"/>
-    <button type="submit">로그인</button>
 </form>
 
 </body>
