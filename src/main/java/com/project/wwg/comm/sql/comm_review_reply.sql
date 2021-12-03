df4bd63b855c5743227561137f12ca4c78a2836f
rysagy <etland6554@gmail.com>
2021-12-03 18:57:10 +0900

    sql문에 종속삭제기능 추가

--- src/main/java/com/project/wwg/comm/sql/comm_review_reply.sql
+++ src/main/java/com/project/wwg/comm/sql/comm_review_reply.sql
@@ -3,7 +3,7 @@
 
 create table comm_review_reply (
 	review_re_no number primary key,         -- 후기게시판 댓글관리 번호
-    review_fno number references comm_review(review_no),  -- foreign key, 후기게시판의 게시글의 번호를 가져온다.
+    review_fno number references comm_review(review_no) on delete cascade,  -- foreign key, 후기게시판의 게시글의 번호를 가져온다.
 	member_id varchar2(50) references users(username),                  -- foreign key
 	review_re_content varchar2(4000) not null,  -- 후기게시판 댓글관리 내용
 	review_re_regdate date not null         -- 후기게시판 댓글관리 생성일자
